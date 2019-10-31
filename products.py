c4237068289b9c8949052a986b2082a61ff264c8
fisurx <davinchocastro@gmail.com>
2019-10-30 23:39:10 -0300

    initial commit

---
+++ products.py
@@ -0,0 +1,5 @@
+products = [
+    {"nombre": "laptop", "price": 32000, "quantity": 4},
+    {"nombre": "monitor", "price": 5000, "quantity": 10},
+    {"nombre": "mouse", "price": 200, "quantity": 20},    
+]
